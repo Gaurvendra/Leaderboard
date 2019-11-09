82d0e9305d055e21cce9baf999c665f9fade6a7d
Gaurvendra <44363966+Gaurvendra@users.noreply.github.com>
2019-11-09 18:58:56 +0530

    Add files via upload

---
+++ Leaderboard.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS Leaderboard (
+    `Mate1` VARCHAR(14) CHARACTER SET utf8,
+    `Mate2` VARCHAR(15) CHARACTER SET utf8,
+    `Score` INT,
+    `Team_No` VARCHAR(2) CHARACTER SET utf8
+);
+INSERT INTO Leaderboard VALUES
+    ('Krishna Bansal','Saaransh Sharma',35,'T1'),
+    ('Anuj Verma','Vasu Tayal',47,'T2'),
+    ('Peeyush Gupta','Gaurav Chandra',25,'T3'),
+    ('Vibhor Singh','Rahul Kumar',0,'T4');
---
+++ index.php
@@ -0,0 +1,147 @@
+<?php
+require 'config.php';
+$que = mysql_query("SELECT * FROM leaderboard ORDER BY Score DESC LIMIT 10");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    
+    <!-- Title Page-->
+    <title>Leaderboard</title>
+
+    <!-- Fontfaces CSS-->
+    <link href="css/font-face.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+
+    <!-- Bootstrap CSS-->
+    <link href="vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <link href="vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+    <link href="vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+    <link href="vendor/wow/animate.css" rel="stylesheet" media="all">
+    <link href="vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+    <link href="vendor/slick/slick.css" rel="stylesheet" media="all">
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/theme.css" rel="stylesheet" media="all">
+
+</head>
+
+<body class="animsition">
+    <div class="page-wrapper">
+        <!-- HEADER DESKTOP-->
+        <header class="header-desktop3 d-none d-lg-block">
+            <div class="section__content section__content--p35">
+               
+            </div>
+        </header>
+     
+        <header class="header-mobile header-mobile-2 d-block d-lg-none">
+            <div class="header-mobile__bar">
+                <div class="container-fluid">
+                    <div class="header-mobile-inner">
+                      
+                    </div>
+                </div>
+            </div>
+           
+            
+        </header>
+        
+
+
+
+            <!-- DATA TABLE-->
+            <section class="p-t-20">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h3 class="title-5 m-b-35">Leaderboard</h3>
+                     
+                            <div class="table-responsive table-responsive-data2">
+                                <table class="table table-data2">
+                                    <thead>
+                                        <tr>
+                                            <th>Team No.</th>
+                                            <th>Mate 1</th>
+                                            <th>Mate 2</th>
+                                            <th>Score</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                while ($row = mysql_fetch_array($que)) {
+                                    ?>
+                                        <tr class="tr-shadow">
+                                           
+                                            <td><?php echo $row['Team_No']; ?></td>
+                                            <td>
+                                                <?php echo $row['Mate1']; ?>
+                                            </td>
+                                            <td><?php echo $row['Mate2']; ?></td>
+                                            <td>
+                                                <?php echo $row['Score']; ?>
+                                            </td>
+                                        </tr>
+                                        <tr class="spacer"></tr> <?php
+                                } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- END DATA TABLE-->
+
+            <!-- COPYRIGHT-->
+            <section class="p-t-60 p-b-20">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="copyright">
+                                <p>Copyright © 2019 UPES-CSI. All rights reserved.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- END COPYRIGHT-->
+        </div>
+
+
+
+    <!-- Jquery JS-->
+    <script src="vendor/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap JS-->
+    <script src="vendor/bootstrap-4.1/popper.min.js"></script>
+    <script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <!-- Vendor JS       -->
+    <script src="vendor/slick/slick.min.js">
+    </script>
+    <script src="vendor/wow/wow.min.js"></script>
+    <script src="vendor/animsition/animsition.min.js"></script>
+    <script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+    </script>
+    <script src="vendor/counter-up/jquery.waypoints.min.js"></script>
+    <script src="vendor/counter-up/jquery.counterup.min.js">
+    </script>
+    <script src="vendor/circle-progress/circle-progress.min.js"></script>
+    <script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="vendor/chartjs/Chart.bundle.min.js"></script>
+    <script src="vendor/select2/select2.min.js">
+    </script>
+
+    <!-- Main JS-->
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
+<!-- end document-->
